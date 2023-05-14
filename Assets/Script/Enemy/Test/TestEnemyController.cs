77696c6eaeac94bc863edba2839d60b19d90650f
suraimu17 <s.r.131117@outlook.jp>
2023-05-15 01:40:13 +0900

    仮配置

---
+++ Assets/Script/Enemy/Test/TestEnemyController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnemyController : MonoBehaviour, IEnemyController
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
