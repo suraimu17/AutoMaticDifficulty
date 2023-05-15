3eb81eda0ed7e189773b3e600d2cc0db4ed24509
suraimu17 <s.r.131117@outlook.jp>
2023-05-16 03:37:47 +0900

    NavMesh2Dの実装と拠点の判定の追加

--- Assets/Script/Enemy/Test/EnemyMover.cs
+++ Assets/Script/Enemy/Test/EnemyMover.cs
@@ -1,12 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using NavMesh2D;
 
 public class EnemyMover : MonoBehaviour
 {
-
+    public NavMeshAgent2D agent;
+    [SerializeField] Transform target;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent2D>();
+    }
     public void EnemyMove()
-    { 
-        
+    {
+        //agent.SetDestination(target.position);
+        agent.destination = target.position;
     }
 }
--- Assets/Script/Enemy/Test/TestEnemyController.cs
+++ Assets/Script/Enemy/Test/TestEnemyController.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Base;
 
 public class TestEnemyController : MonoBehaviour, IEnemyController
 {
-    // Start is called before the first frame update
-    void Start()
+    protected EnemyMover enemyMover;
+    private BaseHP baseHP;
+    private void Start()
     {
-        
+        enemyMover = GetComponent<EnemyMover>();
     }
 
     private void Update()
     {
-        
+        enemyMover.EnemyMove();
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Base") 
+        {
+            Debug.Log("Base");
+            baseHP = collision.GetComponent<BaseHP>();
+            if (baseHP == null) return;
+
+            baseHP.DecreaseHp();
+            Destroy(gameObject);
+        }
+    }
+
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.tag == "Base")
+        {
+            baseHP = collision.GetComponent<BaseHP>();
+            if (baseHP == null) return;
+
+            baseHP.DecreaseHp();
+            Destroy(this);
+        }
+    }*/
 }
