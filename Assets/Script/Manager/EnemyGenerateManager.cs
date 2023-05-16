f621476247aca24f8b2a442f2b67536739c1278c
suraimu17 <s.r.131117@outlook.jp>
2023-05-16 14:52:34 +0900

    修正

---
+++ Assets/Script/Manager/EnemyGenerateManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx.Triggers;
+using UniRx;
+
+public class EnemyGenerateManager : MonoBehaviour
+{
+    [SerializeField] Transform baseTransform;
+    [SerializeField] Transform[] generatePoint;
+    [SerializeField] GameObject enemy;
+
+
+    private void Update()
+    {
+        
+    }
+}
