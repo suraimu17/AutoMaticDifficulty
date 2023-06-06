40e6da74bc9bc1b39cd711e96f6a18851056f458
suraimu17 <s.r.131117@outlook.jp>
2023-06-06 14:22:24 +0900

    修正

---
+++ Assets/Script/Manager/EnemyPattern.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using Cysharp.Threading.Tasks;
+
+public class EnemyPattern : MonoBehaviour
+{
+
+
+
+
+
+    private bool GeneratePattern(GameObject Enemy1,GameObject Enemy2) 
+    {
+        
+
+        return true;
+    }
+
+}
