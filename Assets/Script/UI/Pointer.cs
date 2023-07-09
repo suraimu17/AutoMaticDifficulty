b42bab3ec329da295314070f4024ba809e1cf533
suraimu17 <s.r.131117@outlook.jp>
2023-07-10 02:37:51 +0900

    prefab修正とpointerの視覚化

--- Assets/Script/UI/Pointer.cs
+++ Assets/Script/UI/Pointer.cs
@@ -4,15 +4,14 @@ using UniRx.Triggers;
 using UniRx;
 using UnityEngine.Tilemaps;
 using Manager;
-using UI;
 
 namespace UI
 {
     public class Pointer : MonoBehaviour
     {
-        //[SerializeField] private TileBase tile;
+        [SerializeField] private TileBase tile;
+        [SerializeField] private TileBase baseTile;
         [SerializeField] private Tilemap tilemap;
-        //[SerializeField] private GameObject chara;
 
         private CoinManager coinManager => CoinManager.Instance;
         private CharaManager charaManager => CharaManager.Instance;
@@ -23,10 +22,12 @@ namespace UI
         private GameObject CurrentPutChara;
 
         private List<Vector3> tileList = new List<Vector3>();
+        private Vector3Int pastGrid;
 
         private void Start()
         {
             shopButton = FindObjectOfType<ShopButton>();
+            pastGrid = new Vector3Int(0, 0, 0);
             PointerObservable();
         }
 
@@ -40,10 +41,8 @@ namespace UI
                 {
                     Vector3 mouse_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-                //タイルマップじゃなくてもキャラでも出来そう。
-                Vector3Int grid = tilemap.WorldToCell(mouse_position);
-
-                    Debug.Log(grid);
+                    //タイルマップじゃなくてもキャラでも出来そう。
+                    Vector3Int grid = tilemap.WorldToCell(mouse_position);
                 /*
                 if (tilemap.HasTile(grid)) 
                 {
@@ -69,6 +68,35 @@ namespace UI
                     }
                 })
                 .AddTo(this);
+
+            //選択タイルを表示  TODO:タイルを戻す
+            this.UpdateAsObservable()
+               .Where(_ => shopButton.IsOpen == false)
+               .Subscribe(_ =>
+               {
+                   Vector3 mouse_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                   Vector3Int grid = tilemap.WorldToCell(mouse_position);
+                   if (tilemap.HasTile(pastGrid))
+                   {
+                       if (grid != pastGrid)
+                       {
+                           Debug.Log("戻し");
+                           tilemap.SetTile(pastGrid, baseTile);
+                       }
+                   }
+                   pastGrid = grid;
+
+                   if (tilemap.HasTile(grid))
+                   {
+                       if (tilemap.GetTile(grid)!=tile)
+                       {
+                           Debug.Log("設置");
+                           tilemap.SetTile(grid, tile);
+                       }
+                   }
+
+               })
+               .AddTo(this);
         }
         private bool checkList(Vector3 position)
         {
